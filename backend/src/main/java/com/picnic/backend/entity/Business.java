5e20a9ccbba8259da8e7513eabf7d007d4cb6b5e
shehanprabodya <shehanprabodya870@gmail.com>
2026-04-07 22:39:46 +0530

    update business entity

--- backend/src/main/java/com/picnic/backend/entity/Business.java
+++ backend/src/main/java/com/picnic/backend/entity/Business.java
@@ -5,13 +5,19 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
-
+@Table(name="business")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Business {
 
     @Id
