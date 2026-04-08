0d8f0bcaef83bd14989912ef5a42ad2d8f0574a1
shehanprabodya <shehanprabodya870@gmail.com>
2026-04-08 18:35:40 +0530

    created wishlist entity

--- backend/src/main/java/com/picnic/backend/entity/Wishlist.java
+++ backend/src/main/java/com/picnic/backend/entity/Wishlist.java
@@ -1,4 +1,36 @@
 package com.picnic.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table (name = "wishlist")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Wishlist {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long wishListId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_wishlist_user"))
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "package_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_wishlist_package"))
+    private Travelpackage travelPackage;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+
 }
